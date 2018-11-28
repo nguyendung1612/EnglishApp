bb02bed495be1745455943f60f856258a8c33a63
Dũng Nguyễn <nguyendung@mac-cua-nguyen.local>
2018-11-27 18:25:00 -0800

    Initial Commit

---
+++ EnglishApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnglishApp
+//
+//  Created by Nguyễn Dũng on 27/11/2018.
+//  Copyright © 2018 Nguyễn Dũng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
